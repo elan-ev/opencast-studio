bd1e720493b2703dedb8250e338c0c1b766c3b3a
mlunzena <luniki@gmail.com>
2019-09-25 13:55:43 +0200

    Added loading spinner

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import Modal from 'react-modal';
-
+import './i18n';
 import * as serviceWorker from './serviceWorker';
+import Loading from './loading';
 
+const App = lazy(() => import('./App'));
 
 // bind modal to appElement (http://reactcommunity.org/react-modal/accessibility/)
 Modal.setAppElement('#root');
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Suspense fallback={<Loading />}>
+    <App />
+  </Suspense>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/loading.js
@@ -0,0 +1,98 @@
+//; -*- mode: rjsx;-*-
+import React from 'react';
+import styled, { keyframes } from 'styled-components/macro';
+
+const Loading = props => {
+  return (
+    <div className={props.className}>
+      <svg width="192" height="192">
+        <circle
+          r="82"
+          cx="96"
+          cy="96"
+          fill="none"
+          stroke="#00bdc4"
+          stroke-width="8"
+          stroke-dashoffset="0"
+          stroke-dasharray="515"
+        ></circle>
+        <circle
+          r="82"
+          cx="96"
+          cy="96"
+          fill="none"
+          stroke="white"
+          stroke-width="14"
+          stroke-dashoffset="20"
+          stroke-dasharray="515"
+        ></circle>
+      </svg>
+      <span>Loading...</span>
+    </div>
+  );
+};
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const StyledLoading = styled(Loading)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  width: 12rem;
+  height: 12rem;
+  transition: opacity 0.5s 0.5s, visibility 0s 1s;
+
+  svg {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    opacity: 1;
+    animation: ${rotate} 6s infinite linear;
+  }
+
+  svg circle {
+    position: absolute;
+    top: 0;
+    left: 0;
+    position: absolute;
+  }
+
+  circle:nth-of-type(2) {
+    animation: circleCover1 3s infinite linear;
+    transform-origin: 50% 50%;
+  }
+
+  circle:nth-of-type(3) {
+    transform-origin: 50% 50%;
+    transform: rotateX(180deg) rotateZ(-16deg);
+  }
+
+  span {
+    text-align: center;
+    position: absolute;
+    max-width: 80%;
+    max-height: 80%;
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+    position: absolute;
+    font-style: italic;
+    font-size: 1.25rem;
+    color: #666;
+  }
+`;
+
+export default StyledLoading;
