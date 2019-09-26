bb0fcda4cfd7018828a0a73cdb331ae7d6b0be6b
Marcus Eibrink-Lunzenauer <github@luniki.de>
2019-09-26 21:45:18 +0200

    Updates

---
+++ precache-manifest.5a16ffe02fc6bd9a08c71e7fe6a619e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9e554cd6730fbceeb6ffe1d881c454",
+    "url": "/index.html"
+  },
+  {
+    "revision": "423643a3ab5f4ddf3704",
+    "url": "/static/js/2.95210851.chunk.js"
+  },
+  {
+    "revision": "f446d7b3f5757b785321",
+    "url": "/static/js/3.eb7c51a4.chunk.js"
+  },
+  {
+    "revision": "bb862ee7cebb68b9bf02",
+    "url": "/static/js/4.67444fd4.chunk.js"
+  },
+  {
+    "revision": "c238b9441737a4704049",
+    "url": "/static/js/main.4b80ca64.chunk.js"
+  },
+  {
+    "revision": "68de31474bef5343d58d",
+    "url": "/static/js/runtime-main.3d06bc1b.js"
+  }
+]);
