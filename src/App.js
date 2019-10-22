1968e07aa588deecd296dc080e295ba296dfebf6
Marcus Eibrink-Lunzenauer <github@luniki.de>
2019-10-22 22:24:21 +0200

    Removed stylis-rtl. Not really working out of the box.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 //; -*- mode: rjsx;-*-
 import React, { useState } from 'react';
-import styled, { StyleSheetManager, ThemeProvider } from 'styled-components/macro';
+import styled, { ThemeProvider } from 'styled-components/macro';
 import Modal from 'react-responsive-modal';
 import { useTranslation } from 'react-i18next';
-import stylisRTLPlugin from 'stylis-rtl';
 
 import GlobalStyle from './style/global-style';
 import languages from './languages';
@@ -52,55 +51,49 @@ function App(props) {
     handleCloseUploadSettings();
   };
 
-  const chosenLang = languages.find(({ short }) => short === chosenLanguage);
-  const isRtl = !!(chosenLang && chosenLang.rtl);
-  const stylisPlugins = isRtl ? [stylisRTLPlugin] : [];
-
   return (
-    <StyleSheetManager stylisPlugins={stylisPlugins}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
 
-        <div className={props.className}>
-          <OpencastHeader
-            languages={languages}
-            chosenLanguage={chosenLanguage}
-            onSelectLanguage={selectLanguage}
-          />
+      <div className={props.className}>
+        <OpencastHeader
+          languages={languages}
+          chosenLanguage={chosenLanguage}
+          onSelectLanguage={selectLanguage}
+        />
 
-          <Studio
-            uploadSettings={uploadSettings}
-            handleOpenUploadSettings={handleOpenUploadSettings}
-          />
+        <Studio
+          uploadSettings={uploadSettings}
+          handleOpenUploadSettings={handleOpenUploadSettings}
+        />
 
-          <Footer />
+        <Footer />
 
-          <Modal
-            open={isModalOpen}
-            onClose={handleCloseUploadSettings}
-            ariaLabelledBy="upload-settings-modal-label"
-            center
-            closeOnEsc={false}
-            closeOnOverlayClick={false}
-            showCloseIcon={false}
+        <Modal
+          open={isModalOpen}
+          onClose={handleCloseUploadSettings}
+          ariaLabelledBy="upload-settings-modal-label"
+          center
+          closeOnEsc={false}
+          closeOnOverlayClick={false}
+          showCloseIcon={false}
+        >
+          <div
+            id="upload-settings-modal-label"
+            css={`
+              display: none;
+            `}
           >
-            <div
-              id="upload-settings-modal-label"
-              css={`
-                display: none;
-              `}
-            >
-              {t('upload-settings-modal-title')}
-            </div>
+            {t('upload-settings-modal-title')}
+          </div>
 
-            <UploadSettings
-              uploadSettings={uploadSettings}
-              updateUploadSettings={handleUpdateUploadSettings}
-            />
-          </Modal>
-        </div>
-      </ThemeProvider>
-    </StyleSheetManager>
+          <UploadSettings
+            uploadSettings={uploadSettings}
+            updateUploadSettings={handleUpdateUploadSettings}
+          />
+        </Modal>
+      </div>
+    </ThemeProvider>
   );
 }
 
