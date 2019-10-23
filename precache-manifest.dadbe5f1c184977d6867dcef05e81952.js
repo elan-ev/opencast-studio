35bd900445bb1a8ecb9beb433c8549f26a18be39
Lars Kiesow <lkiesow@uos.de>
2019-10-23 23:21:07 +0200

    Update 2019-10-23

---
+++ precache-manifest.dadbe5f1c184977d6867dcef05e81952.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f948c0a8241c7cc47e462632c906990f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b8139f74c376c2262bf",
+    "url": "/static/js/2.082418ef.chunk.js"
+  },
+  {
+    "revision": "868a309c4700c4854071",
+    "url": "/static/js/3.02df242f.chunk.js"
+  },
+  {
+    "revision": "8a3a043276a51381ad29",
+    "url": "/static/js/4.67044919.chunk.js"
+  },
+  {
+    "revision": "2e57e10630724e90c87d",
+    "url": "/static/js/main.214a3251.chunk.js"
+  },
+  {
+    "revision": "4fd6eed06c3ae655a87c",
+    "url": "/static/js/runtime-main.2f3ce6ba.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c7d650198e98c64bddadb5467e9eb1b7.js"
+  "/precache-manifest.dadbe5f1c184977d6867dcef05e81952.js"
 );
 
 self.addEventListener('message', (event) => {
