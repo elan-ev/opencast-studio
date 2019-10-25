9fc34801be9d989d712b1732c3f020c1cec4fbb5
Travis CI User <travis@example.org>
2019-10-25 20:11:04 +0000

    Build #331 (Fri Oct 25 20:11:04 UTC 2019) | 3be37cb Fix Deployment

---
+++ precache-manifest.2d81b051802bb0a32fec00d22671691e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ebbf666e794f545d2d47e2d1744c7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b8139f74c376c2262bf",
+    "url": "/static/js/2.082418ef.chunk.js"
+  },
+  {
+    "revision": "868a309c4700c4854071",
+    "url": "/static/js/3.02df242f.chunk.js"
+  },
+  {
+    "revision": "b987c07f43cc4ac871cf",
+    "url": "/static/js/4.d38ac663.chunk.js"
+  },
+  {
+    "revision": "73e28b8a51168914db9c",
+    "url": "/static/js/main.86d87721.chunk.js"
+  },
+  {
+    "revision": "2938765a7cc54f1e9605",
+    "url": "/static/js/runtime-main.81590f31.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
