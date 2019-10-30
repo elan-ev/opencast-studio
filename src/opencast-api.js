c475dd27983ed9d5e04e73a2bd36ff0539ce42cb
Marcus Eibrink-Lunzenauer <github@luniki.de>
2019-10-30 06:47:02 +0100

    Used axios instead of self implemented XmlHttpRequest wrapper.

--- src/opencast-api.js
+++ src/opencast-api.js
@@ -1,46 +1,4 @@
-function xhr(url, opts) {
-  return new Promise((resolve, reject) => {
-    if (!url) {
-      reject('no url');
-      return;
-    }
-
-    const req = new XMLHttpRequest();
-    const reqType = (opts ? opts.type || 'GET' : 'GET').toUpperCase();
-    req.open(reqType, url, true);
-    if (opts && opts.hasOwnProperty('attributes')) {
-      for (let key in opts.attributes) {
-        req.setAttribute(key, opts.attributes[key]);
-      }
-    }
-
-    if (opts && opts.hasOwnProperty('properties')) {
-      for (let key in opts.properties) {
-        req[key] = opts.properties[key];
-      }
-    }
-
-    if (opts && opts.hasOwnProperty('requestHeaders')) {
-      for (let key in opts.requestHeaders) {
-        req.setRequestHeader(key, opts.requestHeaders[key]);
-      }
-    }
-
-    req.onload = e => {
-      if (req.status === 200) {
-        resolve(req.response);
-      } else {
-        reject(e);
-      }
-    };
-
-    req.onerror = e => {
-      reject(e);
-    };
-
-    req.send(opts && opts.data ? opts.data : null);
-  });
-}
+import axios from 'axios';
 
 class OpencastAPI {
   constructor(settings) {
@@ -76,7 +34,6 @@ class OpencastAPI {
     try {
       isLoggedIn = await this.validateState();
     } catch (e) {
-      debugger;
       onserverunreachable(e);
       return;
     }
@@ -119,29 +76,28 @@ class OpencastAPI {
 
     try {
       let mp = await this.getMediaPackage().then(mp => {
-        let base_dc =
-          '<?xml version="1.0" encoding="UTF-8"?>' +
-          '<dublincore xmlns="http://www.opencastproject.org/xsd/1.0/dublincore/"' +
-          '            xmlns:dcterms="http://purl.org/dc/terms/"' +
-          '            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">' +
-          '  <dcterms:created xsi:type="dcterms:W3CDTF">2001-01-01T01:01Z</dcterms:created>' +
-          '  <dcterms:creator>Creator not set</dcterms:creator>' +
-          '  <dcterms:extent xsi:type="dcterms:ISO8601">PT5.568S</dcterms:extent>' +
-          '  <dcterms:title>Title Not Set</dcterms:title>' +
-          '  <dcterms:spatial>Opencast Studio</dcterms:spatial>' +
-          '</dublincore>';
+        let base_dc = `<?xml version="1.0" encoding="UTF-8"?>
+          <dublincore xmlns="http://www.opencastproject.org/xsd/1.0/dublincore/"
+                      xmlns:dcterms="http://purl.org/dc/terms/"
+                      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+              <dcterms:created xsi:type="dcterms:W3CDTF">2001-01-01T01:01Z</dcterms:created>
+              <dcterms:creator>Creator not set</dcterms:creator>
+              <dcterms:extent xsi:type="dcterms:ISO8601">PT5.568S</dcterms:extent>
+              <dcterms:title>Title Not Set</dcterms:title>
+              <dcterms:spatial>Opencast Studio</dcterms:spatial>
+          </dublincore>`;
 
-        let dc = new DOMParser().parseFromString(base_dc, 'text/xml');
-        let dc_created = dc.getElementsByTagName('dcterms:created');
-        let dc_creator = dc.getElementsByTagName('dcterms:creator');
-        let dc_title = dc.getElementsByTagName('dcterms:title');
+        const dc = new DOMParser().parseFromString(base_dc, 'text/xml');
+        const dc_created = dc.getElementsByTagName('dcterms:created');
+        const dc_creator = dc.getElementsByTagName('dcterms:creator');
+        const dc_title = dc.getElementsByTagName('dcterms:title');
 
         dc_created[0].textContent = new Date(Date.now()).toISOString();
         dc_creator[0].textContent = creator;
         dc_title[0].textContent = title;
 
         return this.addDCCatalog(
-          mp,
+          mp.data,
           new XMLSerializer().serializeToString(dc),
           'dublincore/episode'
         );
@@ -161,94 +117,77 @@ class OpencastAPI {
         const flavor = deviceType === 'desktop' ? 'Presentation' : 'Presenter';
         const downloadName = `${flavor} ${type} - ${title || 'Recording'}.webm`;
 
-        mp = await this.addTrack(mp, media, downloadName, trackFlavor, '');
+        mp = await this.addTrack(mp.data, media, downloadName, trackFlavor, '');
       }
 
-      mp = await this.ingest(mp, this.workflow_id);
+      await this.ingest(mp.data, this.workflow_id);
     } catch (err) {
       // Inet failed (while transmitting) or not enough permission
       oninetorpermfailed(err);
-      debugger;
       return;
     }
 
     onsuccess();
-
-    return;
   }
 
   async validateState() {
-    let me_info_json = await this.getMeInfo();
-    let me_info = JSON.parse(me_info_json);
+    const me_info_json = await this.getMeInfo();
+    const me_info = me_info_json.data;
 
-    // ret isLoggedIn
     return me_info['user']['username'] !== 'anonymous';
     // OnError: Couldn't connect to server: inet failed / is not a Opencast Api / CORS not allowed
   }
 
   login() {
-    let data =
+    const data =
       'j_username=' +
       this.login_name +
       '&j_password=' +
       this.login_password +
       '&_spring_security_remember_me=on';
 
-    return this.cred_xhr(`${this.server_url}/admin_ng/j_spring_security_check`, 'POST', data, true);
+    const url = `${this.server_url}/admin_ng/j_spring_security_check`;
+    return axios({ url, method: 'post', data, withCredentials: true });
   }
 
   getMeInfo() {
-    return this.cred_xhr(`${this.server_url}/info/me.json`);
+    const url = `${this.server_url}/info/me.json`;
+    return axios.get(url, { responseType: 'text', withCredentials: true });
   }
 
   getMediaPackage() {
-    return this.cred_xhr(`${this.server_url}/ingest/createMediaPackage`);
+    const url = `${this.server_url}/ingest/createMediaPackage`;
+    return axios.get(url, { responseType: 'text', withCredentials: true });
   }
 
   addDCCatalog(mp, dc, flavor) {
-    let data = new FormData();
+    const data = new FormData();
     data.append('mediaPackage', mp);
     data.append('dublinCore', dc);
     data.append('flavor', flavor);
 
-    return this.cred_xhr(`${this.server_url}/ingest/addDCCatalog`, 'POST', data);
+    const url = `${this.server_url}/ingest/addDCCatalog`;
+    return axios({ url, method: 'post', data, responseType: 'text', withCredentials: true });
   }
 
   addTrack(mp, track, track_filename, flavor, tags) {
-    let data = new FormData();
+    const data = new FormData();
     data.append('mediaPackage', mp);
     data.append('flavor', flavor);
     data.append('tags', tags);
     data.append('BODY', track, track_filename);
+    const url = `${this.server_url}/ingest/addTrack`;
 
-    return this.cred_xhr(`${this.server_url}/ingest/addTrack`, 'POST', data);
+    return axios({ url, method: 'post', data, responseType: 'text', withCredentials: true });
   }
 
   ingest(mp, workflowId) {
-    let data = new FormData();
+    const data = new FormData();
     data.append('mediaPackage', mp);
     data.append('workflowDefinitionId', workflowId);
+    const url = `${this.server_url}/ingest/ingest`;
 
-    return this.cred_xhr(`${this.server_url}/ingest/ingest`, 'POST', data);
-  }
-
-  cred_xhr(url, type, data, isUrlencoded) {
-    let opts = {
-      properties: {
-        withCredentials: true,
-        responseType: 'text'
-      }
-    };
-
-    opts.type = type;
-    opts.data = data;
-    opts.requestHeaders = {};
-
-    if (isUrlencoded) {
-      opts.requestHeaders['Content-Type'] = 'application/x-www-form-urlencoded';
-    }
-
-    return xhr(url, opts);
+    return axios({ url, method: 'post', data, responseType: 'text', withCredentials: true });
   }
 }
 
