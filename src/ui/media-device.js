9c9fac48d679df5c475d26fde3bf695ab762cbe2
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2019-11-13 17:14:48 +0100

    fix: cropped preview video
    Now the full preview video stream is always visible. However, due to
    the current layout, this might result in a lot of wasted screen space.
    This flaw with the layout will be fixed later.

--- src/ui/media-device.js
+++ src/ui/media-device.js
@@ -37,6 +37,7 @@ function MediaDevice({ onClick, title, deviceType, icon, stream }) {
           top: '0',
           left: '0',
           width: '100%',
+          height: '100%',
           zIndex: '2',
           background: 'transparent'
         }}
