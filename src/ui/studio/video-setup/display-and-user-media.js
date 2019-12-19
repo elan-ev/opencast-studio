1ccbe7641331e04243efe7087961f861dff62dac
Marcus Eibrink-Lunzenauer <github@luniki.de>
2019-12-19 09:54:07 +0100

    refactor: Add better usage descriptions to video setup.
    The descriptions still need better text, but for now the intention is clearer.

--- src/ui/studio/video-setup/display-and-user-media.js
+++ src/ui/studio/video-setup/display-and-user-media.js
@@ -79,9 +79,7 @@ export default function SourceDisplayAndUserMedia() {
       {!(state.displayStream || state.userStream) ? (
         <Box>
           <Text pb={4}>
-            {
-              'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-            }
+            {t('source-display-and-user-usage')}
           </Text>
           <ShareButton handleClick={handleShare} icon={faChalkboardTeacher}>
             {t('source-share-prompt')}
