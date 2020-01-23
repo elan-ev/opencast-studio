5b0aaeed07c3cbcf17759fb14ba297c621cb005c
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2020-01-23 16:59:10 +0100

    Improve `Warnings` to avoid useless gap in case of no warnings

--- src/ui/warnings.js
+++ src/ui/warnings.js
@@ -1,7 +1,6 @@
 //; -*- mode: rjsx;-*-
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
-import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import Notification from './notification';
@@ -15,34 +14,42 @@ import {
 const Warnings = ({ settings }) => {
   const { t } = useTranslation();
 
+  let warnings = [];
+
   // We allow HTTP connections to localhost, as most browsers also seem to allow
   // video capture in those cases.
   const usingUnsecureConnection = window.location.protocol !== 'https:' &&
     window.location.hostname !== "localhost" &&
     window.location.hostname !== "127.0.0.1";
+  if (usingUnsecureConnection) {
+    warnings.push(
+      <Notification isDanger>{t('warning-https')}</Notification>
+    );
+  }
 
-  const opencastConfigured = OpencastAPI.areSettingsComplete(settings.opencast);
+  // Warning about missing `MediaRecorder` support
+  if (!isRecordingSupported()) {
+    warnings.push(
+      <Notification isDanger>
+        {t('warning-recorder-not-supported')}
+        {onSafari() && ' ' + t('warning-recorder-safari-hint')}
+      </Notification>
+    );
+  }
 
-  return (
-    <div sx={{ p: 3 }}>
-      {!isRecordingSupported() && (
-        <Notification isDanger>
-          {t('warning-recorder-not-supported')}
-          {onSafari() && ' ' + t('warning-recorder-safari-hint')}
-        </Notification>
-      )}
+  // Warn if the user has not yet configured the Opencast connection
+  if (!OpencastAPI.areSettingsComplete(settings.opencast)) {
+    warnings.push(
+      <Notification isDanger>
+        {t('warning-missing-connection-settings')}
+      </Notification>
+    );
+  }
 
-      { usingUnsecureConnection && (
-        <Notification isDanger>{t('warning-https')}</Notification>
-      )}
 
-      { !opencastConfigured && (
-        <Notification isDanger>
-          {t('warning-missing-connection-settings')}
-        </Notification>
-      )}
-    </div>
-  );
+  return warnings.length > 0
+    ? <div sx={{ p: 3 }}>{ warnings }</div>
+    : null;
 };
 
 export default Warnings;
