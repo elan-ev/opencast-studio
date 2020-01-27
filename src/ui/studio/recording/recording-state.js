a922d8ae38d6b0b3a1e01f29c637c5cd19af0a32
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2020-01-27 17:36:40 +0100

    Center recording button and remove unused text status
    The text status was invisible. I doubt it's useful for anything
    anymore.

--- src/ui/studio/recording/recording-state.js
+++ src/ui/studio/recording/recording-state.js
@@ -2,21 +2,12 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 
-import { useTranslation } from 'react-i18next';
 import Clock from './clock';
 
 const RecordingState = props => {
-  const { t } = useTranslation();
   return (
       <div sx={{ display: "flex", alignItems: 'center', visibility: props.recordingState === 'inactive' ? 'hidden' : 'visible' }}>
       <Clock recordingState={props.recordingState} />
-        <p sx={{ px: 2, color: props.recordingState === 'paused' ? 'gray.1' : 'transparent' }}>
-        {props.recordingState === 'recording'
-          ? t('Recording')
-          : props.recordingState === 'paused'
-          ? t('Paused')
-          : t('Waiting')}
-      </p>
     </div>
   );
 };
