605ac8f0ddf8769e9cfbd719ca3cbb437a2746a7
Marcus Eibrink-Lunzenauer <github@luniki.de>
2020-01-28 09:56:24 +0100

    refactor: replace react-async

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 //; -*- mode: rjsx;-*-
 /** @jsx jsx */
-import { jsx } from 'theme-ui';
-
+import { jsx, ThemeProvider } from 'theme-ui';
 
 import { Global } from '@emotion/core';
-import ReactDOM from 'react-dom';
 import * as Sentry from '@sentry/browser';
-import { ThemeProvider } from 'theme-ui';
-import { useAsync } from "react-async"
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 
 import GlobalStyle from './style/global-style';
 import theme from './theme';
@@ -18,28 +16,35 @@ import Loading from './loading';
 import { SettingsManager } from './settings';
 
 if (process.env.NODE_ENV === 'production') {
-    Sentry.init({ dsn: 'https://66e6b4dc3d59463fa34272abcb5da6b1@sentry.virtuos.uos.de/4' });
+  Sentry.init({ dsn: 'https://66e6b4dc3d59463fa34272abcb5da6b1@sentry.virtuos.uos.de/4' });
 }
-
 
 // This async function does all the heavy initialization of the app.
-const init = async () => {
+const init = () => {
   const module = import('./App').then(module => module.default);
   return Promise.all([module, SettingsManager.init()]);
-}
+};
 
 // This component shows a loading screen until `init` is finished; then the main
 // app is shown.
 const LoadingApp = () => {
-  const { data, error, isPending } = useAsync({ promiseFn: init });
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isPending, setPending] = useState(true);
+
+  useEffect(() => {
+    init()
+      .then(setData)
+      .catch(setError)
+      .finally(() => setPending(false));
+  }, []);
 
   if (isPending) {
     return <Loading />;
   }
 
   if (error) {
-    // TODO: not sure what to do here
-    return `Something went wrong: ${error.message}`
+    return `Something went wrong: ${error.message}`;
   }
 
   let [App, settingsManager] = data;
@@ -47,13 +52,14 @@ const LoadingApp = () => {
 };
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <Global styles={GlobalStyle} />
-  <LoadingApp />
-  </ThemeProvider>,
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <Global styles={GlobalStyle} />
+      <LoadingApp />
+    </ThemeProvider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
