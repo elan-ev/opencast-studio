68eae118d7312c69731bed92c09b0ebb447190a7
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2020-02-20 13:54:01 +0100

    Fix position of loading indicator on large screens

--- src/style/global-style.js
+++ src/style/global-style.js
@@ -5,13 +5,6 @@ const GlobalStyle = css`
     box-sizing: border-box;
 }
 
-html, body {
-    margin: 0;
-    padding: 0;
-    height: 100%;
-    min-height: 100%;
-}
-
 html, body, button, input {
     font-family: Ubuntu, Roboto, "Open Sans", "Segoe UI", "Helvetica Neue", Verdana, sans-serif;
 }
