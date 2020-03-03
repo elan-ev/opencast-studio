20cc1c9917474b2fb4523a603dbc08b01388b806
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2020-03-03 15:04:30 +0100

    Add video synchronization in review step
    Both videos always keep the same `currentTime` now, with a difference
    below 150ms, usually much lower.

--- src/ui/studio/review/index.js
+++ src/ui/studio/review/index.js
@@ -2,6 +2,7 @@
 /** @jsx jsx */
 import { jsx, Styled } from 'theme-ui';
 
+import React, { useEffect } from 'react';
 import { Flex, Spinner } from '@theme-ui/components';
 import { useTranslation } from 'react-i18next';
 import { Beforeunload } from 'react-beforeunload';
@@ -52,6 +53,89 @@ const Preview = () => {
   const { recordings } = useRecordingState();
   const { t } = useTranslation();
 
+  const videoRefs = [React.createRef(), React.createRef()];
+  const desktopIndex = recordings.length === 2
+    ? (recordings[0].deviceType === 'desktop' ? 0 : 1)
+    : null;
+
+  // Setup synchronization between both video elements
+  useEffect(() => {
+    if (recordings.length === 2 && videoRefs[0].current && videoRefs[1].current) {
+      // If we have two recordings, both will have audio. But the user doesn't
+      // want to hear audio twice, so we mute one video element. Particularly,
+      // we mute the desktop video, as there the audio/video synchronization is
+      // not as critical.
+      videoRefs[desktopIndex].current.volume = 0;
+
+      // The index of the last video ref that received an event (0 or 1).
+      let lastOrigin = null;
+
+      const va = videoRefs[0].current;
+      const vb = videoRefs[1].current;
+
+      // Helper function to add event listener to both video streams. Assumes that
+      // the refs are valid, i.e. `current` is pointing to the video element.
+      const installEventHandlers = (eventName, handler) => {
+        // Stores if the event was triggered by us (the code) as opposed to "by
+        // the user". This always flips as the user triggers the first event and
+        // we always react with one action that triggers one event.
+        let weTriggered = false;
+
+        // Add event listener for both video elements
+        va.addEventListener(eventName, event => {
+          if (!weTriggered) {
+            lastOrigin = 0;
+            handler(event, va, vb);
+          }
+
+          weTriggered = !weTriggered;
+        });
+        vb.addEventListener(eventName, event => {
+          if (!weTriggered) {
+            lastOrigin = 1;
+            handler(event, vb, va);
+          }
+
+          weTriggered = !weTriggered;
+        });
+      };
+
+      // Actuall install the handlers for different events
+      installEventHandlers('play', (event, origin, target) => {
+        target.currentTime = origin.currentTime;
+        target.play();
+      });
+      installEventHandlers('pause', (event, origin, target) => {
+        target.currentTime = origin.currentTime;
+        target.pause();
+      });
+      installEventHandlers('seeking', (event, origin, target) => {
+        target.currentTime = origin.currentTime;
+      });
+
+      // Install backup synchronization that runs regularly.
+      let frameCounter = 0;
+      const fixTime = () => {
+        // Only run every 60 frames.
+        if (frameCounter % 60 === 0) {
+          // We want the difference to be below 150ms. Usually, even without
+          // this backup solution, it should be below 50ms at all time. That's
+          // what testing showed.
+          const diff = Math.abs(va.currentTime - vb.currentTime);
+          if (diff > 0.15 && lastOrigin) {
+            const origin = videoRefs[lastOrigin].current;
+            const target = videoRefs[lastOrigin === 0 ? 1 : 0].current;
+            target.currentTime = origin.currentTime;
+          }
+        }
+
+        frameCounter++;
+        window.requestAnimationFrame(fixTime);
+      };
+      window.requestAnimationFrame(fixTime);
+    }
+  });
+
   if (recordings.length === 0) {
     return <Spinner title={t('save-creation-waiting-for-recordings')} />;
   }
@@ -59,6 +143,7 @@ const Preview = () => {
   const children = recordings.map((recording, index) => ({
     body: (
       <video
+        ref={videoRefs[index]}
         key={index}
         controls
         src={recording.url}
