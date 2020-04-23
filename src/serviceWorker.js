6838ce36b4f63ddfe0bdde7da05ee41afde4cbda
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2020-04-23 09:27:46 +0200

    Fix issue in service worker handling that causes Firefox error
    See here for more details:
    - https://github.com/facebook/create-react-app/pull/8272
    - https://github.com/facebook/create-react-app/issues/8047
    
    Since this is fixed in the template, updating CRA doesn't help and we
    have to apply the patch to our file manually. This hopefully fixes an
    error we got in Sentry a lot (roughly 1.5k events right now).

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -128,8 +128,12 @@ function checkValidServiceWorker(swUrl, config) {
 
 export function unregister() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(registration => {
-      registration.unregister();
-    });
+    navigator.serviceWorker.ready
+      .then(registration => {
+        registration.unregister();
+      })
+      .catch(error => {
+        console.error(error.message);
+      });
   }
 }
