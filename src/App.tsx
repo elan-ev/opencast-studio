dc0ba9f9a097b920fa9c73b079a91ccfd7ef4aac
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2023-08-01 19:03:56 +0200

    Add logic to prevent closing the tab if there are unsaved recordings

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,34 @@ import { Root } from "./layout";
 
 import { ColorSchemeProvider } from "@opencast/appkit";
 import { GlobalStyle } from "./ui/global-style";
+import { useStudioState } from "./studio-state";
+import { useBeforeunload } from "react-beforeunload";
 
 
 
 export const App: React.FC = () => {
   return <>
     <GlobalStyle />
+    <PreventClose />
     <ColorSchemeProvider>
       <Root />
     </ColorSchemeProvider>
   </>;
 };
+
+/** Prevent closing the tab if recordings exists and they have not been saved. */
+const PreventClose = () => {
+  const { recordings, upload } = useStudioState();
+  const downloaded = recordings.every(rec => rec.downloaded);
+  const uploaded = upload.state === "uploaded";
+  const uploading = upload.state === "uploading";
+
+  useBeforeunload(event => {
+    if ((recordings?.length > 0 && !uploaded && !downloaded) || uploading) {
+      event.preventDefault();
+    }
+  });
+
+  return null;
+};
+
