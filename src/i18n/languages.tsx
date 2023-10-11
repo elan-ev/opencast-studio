de6645ab419faf43905f0b3a3317d8083f3b2bef
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2023-10-11 16:49:29 +0200

    Make all language use their native language label in menu
    That is recommended for fairly obvious reasons:
    https://ux.stackexchange.com/q/37017/138418
    
    Studio was inconsistent about this before.

--- src/i18n/languages.tsx
+++ src/i18n/languages.tsx
@@ -1,9 +1,9 @@
 const languages = [
   { short: "en", long: "English", rtl: false },
   { short: "de", long: "Deutsch", rtl: false },
-  { short: "nl", long: "Dutch", rtl: false },
+  { short: "nl", long: "Nederlands", rtl: false },
   { short: "es", long: "Español", rtl: false },
-  { short: "fa", long: "Farsi", rtl: true },
+  { short: "fa", long: "فارسی", rtl: true },
   { short: "fr", long: "Français", rtl: false },
   { short: "sl", long: "Slovenščina", rtl: false },
   { short: "zh", long: "简体中文", rtl: false },
